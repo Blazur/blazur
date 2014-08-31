aee76feb5627cb8d4a97aa163ecbcad3483c4452
Hendrixer <willscottmoss@gmail.com>
2014-08-30 23:47:28 -0700

    link up server, add ripple, add styles

--- app/scripts/home/home.js
+++ app/scripts/home/home.js
@@ -14,6 +14,102 @@
     'classy'
   ])
   .config(configBlock)
+  .directive('ripple', function() {
+    function rippleLinkFn(scope, element) {
+      element.on('mousedown', function(e) {
+        var touch  = angular.element('<div></div>');
+
+        var size = element[0].clientWidth * 1.9;
+        var complete = false;
+
+        angular.element(document)
+        .on('mouseup', function() {
+          var a = {
+            'opacity': '0'
+          };
+
+          if (complete) {
+            size *= 1.2;
+            angular.extend(a, {
+              'height': size + 'px',
+              'width': size + 'px',
+              'marginTop': -(size)/2 + 'px',
+              'marginLeft': -(size)/2 + 'px',
+              'ease': Sine.easeIn,
+              onComplete: function(){
+                touch.remove();
+              }
+            });
+          }
+
+          TweenMax.to(touch, .5, a);
+        });
+
+        touch.addClass('touch');
+        touch.css({
+          'position': 'absolute',
+          'top': e.pageY-element[0].getBoundingClientRect().top + 'px',
+          'left': e.pageX-element[0].getBoundingClientRect().left + 'px',
+          'width': '0',
+          'height': '0'
+        });
+
+        element.append(touch);
+
+        TweenMax.to(touch, .5, {
+          'height': size + 'px',
+          'width': size + 'px',
+          'margin-top': -(size)/2 + 'px',
+          'margin-left': -(size)/2 + 'px',
+          'ease': Quart.easeOut,
+          onComplete: function() {
+            complete = true;
+          }
+        });
+      });
+    }
+    return rippleLinkFn;
+  })
+  .directive('drawer', [function() {
+    function drawerLinkFn(scope, elem) {
+      var kids = elem.children();
+
+      var navdrawerContainer = angular.element(kids[1]);
+      var body = document.body;
+      var appbarElement = angular.element(kids[0]);
+      var menuBtn = appbarElement.find('button');
+      var main = angular.element(kids[2]);
+
+      function closeMenu() {
+        body.classList.remove('open');
+        appbarElement.removeClass('open');
+        navdrawerContainer.removeClass('open');
+      }
+
+      function toggleMenu() {
+        body.classList.toggle('open');
+        appbarElement.toggleClass('open');
+        navdrawerContainer.toggleClass('open');
+        navdrawerContainer.addClass('opened');
+      }
+
+      main.on('click', closeMenu);
+      menuBtn.on('click', toggleMenu);
+      navdrawerContainer.on('click', function (event) {
+        if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+          closeMenu();
+        }
+      });
+
+      scope.$on('$destroy', function() {
+        main.off('click');
+        menuBtn.off('click');
+        navdrawerContainer.off('click');
+      });
+    }
+
+    return drawerLinkFn;
+  }])
   .classy.controller({
     name: 'HomeController',
 
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var express = require('express');
+'use strict';
 
-var app = express();
-app.use(express.static('bower_components'));
-app.set('port', process.env.PORT || 4000);
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.static('.tmp'));
-  app.use(express.static('app'));
+var app     = require('./server/main.js'),
+    server  = app.listen(app.get('port')),
+    ioAuth  = require('socketio-jwt'),
+    io      = require('socket.io').listen(server),
+    secret  = require('./server/config/config').jwt;
 
-} else {
-  app.use(express.static('dist'));
-}
+// io.set('authorization', ioAuth.authorize({
+//   secret: secret,
+//   handshake: true
+// }));
 
+io.sockets.on('connection', function(socket) {
+  require('./server/api/user/socket/routes')(socket, io);
+});
 
-app.listen(app.get('port'));
-
-console.log('on port 4000');
+console.log('on port ', app.get('port'));
