3b9ec5cae6f943abf09e8e78a5cb193295518078
Hendrixer <willscottmoss@gmail.com>
2014-08-31 23:33:41 -0700

    add build files

--- app/scripts/home/home.js
+++ app/scripts/home/home.js
@@ -174,7 +174,7 @@
           'margin-left': -(size)/2 + 'px',
           'ease': Expo.easeOut
         })
-        .to(touch, rippleDuration, { opacity: '0', onComplete:function(){touch.remove();} }, 0)
+        .to(touch, rippleDuration, { opacity: '0', onComplete:function(){touch.remove();} }, 0);
 
         angular.element(document.body).find('nav').css('border-top', '2px solid ' + borderHash[newVal]);
         child = element.children()[0];
