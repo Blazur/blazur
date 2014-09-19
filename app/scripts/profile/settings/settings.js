867d3a11c091959e608d07ba513c664830acfd25
Mehul Patel <mehul8@gmail.com>
2014-09-18 22:48:46 -0700

    refactor(settings.js): scaffold out creditCard directive

--- app/scripts/profile/settings/settings.js
+++ app/scripts/profile/settings/settings.js
@@ -19,6 +19,38 @@
 
   angular.module('app.profile.settings', ['classy'])
   .config(configBlock)
+  .directive('creditCard', [function() {
+    return {
+      restrict: 'EA',
+      replace: true,
+      templateUrl: 'credit-card.html',
+      scope: {},
+      link: function(scope, element, attr) {
+        scope.creditCard = {
+          stage: ''
+        };
+
+        var input = element.find('input');
+
+        input.on('focus', function() {
+          TweenMax.to(element, 0.5, { height: '500px', ease: Strong.easeInOut, onComplete: function() {
+            scope.ready = true;
+            scope.$apply();
+          } });
+        });
+
+        input.on('blur', function() {
+          if (scope.creditCard.stuff) {
+            return;
+          }
+
+          scope.ready = false;
+          scope.$apply();
+          TweenMax.to(element, 0.5, { height: '62px', ease: Strong.easeInOut });
+        });
+      }
+    };
+  }])
   .classy.controller({
     name: 'SettingsController',
     init: function() {
